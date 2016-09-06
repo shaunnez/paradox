ec6895bcbe38003cb670adf2079f61f6b8940b25
Shaun Nesbitt <shaun.nesbitt@airnz.co.nz>
2016-09-06 14:38:30 +1200

    push

--- app/js/app.js
+++
@@ -1,8 +0,0 @@
-// var a = require('../index.handlebars');
-
-import '../css/app.scss';
-import $ from "jquery";
-// import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-
-// import 'waypoints';
-// import 'scrollTo';
